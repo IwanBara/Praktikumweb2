d4373cd019fada4d80975b9cf589ee7ba15adf21
IwanBara <91892202+IwanBara@users.noreply.github.com>
2022-04-21 15:41:22 +0700

    Add files via upload

---
+++ project1/class_BMIpasien.php
@@ -0,0 +1,14 @@
+<?php
+class BMIpasien{
+    public $bmi;
+    public $tanggal;
+    public $pasien;
+
+    function __construct($bmi, $tanggal, $pasien){
+        $this->bmi;
+        $this->tanggal;
+        $this->pasien;
+    }
+
+}
+?>
