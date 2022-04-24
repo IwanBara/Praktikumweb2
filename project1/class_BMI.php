63236f2833810bcdeec1c5ce1c16aaccd848214b
IwanBara <91892202+IwanBara@users.noreply.github.com>
2022-04-24 11:50:10 +0700

    Add files via upload

---
+++ project1/class_BMI.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'class_BMIpasien.php';
+
+class BMI extends BMIpasien{
+    public $berat;
+    public $tinggi;
+    
+    public function __construct($berat,$tinggi){
+        $this->berat = $berat;
+        $this->tinggi = $tinggi;
+    }
+
+    function nilaiBMI(){
+        $tingg_meter = $this->tinggi/100;
+        $this->nilaiBMI =$this->berat / ($tingg_meter * $tingg_meter);
+        return $this->nilaiBMI = number_format($this->nilaiBMI,1);
+    }
+
+    public function statusBMI(){
+        if ($this->nilaiBMI < 18.5){
+            return 'Kekurangan berat badan';
+        }
+        else if ($this->nilaiBMI <=18.5){
+            return 'Normal';
+        }
+        else if ($this->nilaiBMI <=25.0){
+            return 'Kelebihan berat badan';
+        }
+        else{
+            return 'Kegemukan';
+        }
+    }
+}
+?>
