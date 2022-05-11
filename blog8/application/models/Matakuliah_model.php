a892c675c884163dbfc96e804a11f578675dda34
IwanBara <iwan21226si@student.nurulfikri.ac.id>
2022-05-11 16:12:53 +0700

    Praktikumweb2

---
+++ blog8/application/models/Matakuliah_model.php
@@ -0,0 +1,17 @@
+<?php
+class Matakuliah_model extends CI_Model{
+    // Buat Property atau variable
+    public $nama, $sks, $kode;
+
+    public function getAll(){
+        // menampilkan seluruh data yang ada di table matakuliah menggunakan query builder
+        $query = $this->db->get('matakuliah');
+        return $query->result();
+    }
+    public function getById($id){
+        // menampilkan data berdasarkan id
+        $query = $this->db->get_where('matakuliah', ['id' => $id]);
+        return $query->row();
+    }
+}
+?>
